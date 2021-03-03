c1ff5d4d78f8f41183fadbbc12e403f3f2f68e7b
Tony Quetano <tony.quetano@planttheidea.com>
2021-03-02 22:32:31 -0800

    actually split build scripts, and replace propTypes with empty object in prod

--- rollup.config.prod.js
+++ rollup.config.prod.js
@@ -2,16 +2,19 @@ import { terser } from 'rollup-plugin-terser';
 import pkg from './package.json';
 import { DEFAULT_CONFIG, DEFAULT_OUTPUT } from './rollup.config';
 
-export default [
-  DEFAULT_CONFIG,
-  {
-    ...DEFAULT_CONFIG,
-    output: {
-      ...DEFAULT_OUTPUT,
-      file: pkg.browser.replace('.js', '.min.js'),
-      format: 'umd',
-    },
-    plugins: [...DEFAULT_CONFIG.plugins],
-    // plugins: [...DEFAULT_CONFIG.plugins, terser()],
+export default {
+  ...DEFAULT_CONFIG,
+  output: {
+    ...DEFAULT_OUTPUT,
+    file: pkg.browser.replace('.js', '.min.js'),
+    format: 'umd',
   },
-];
+  plugins: [
+    ...DEFAULT_CONFIG.plugins,
+    terser({
+      compress: { passes: 3 },
+      mangle: false,
+      format: { beautify: true },
+    }),
+  ],
+};
